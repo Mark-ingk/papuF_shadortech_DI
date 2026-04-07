fa37f53c65c85792cbc23ad9dc695b06c5a82995
Arty <artimexx171@gmail.com>
2026-04-07 14:05:41 -0600

    hice el diseño inicial de la interfaz de perfil :V

--- appPapuaFit/frmPerfil.vb
+++ appPapuaFit/frmPerfil.vb
@@ -3,4 +3,23 @@
 
 
     End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged_1(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Guna2TextBox3_TextChanged(sender As Object, e As EventArgs) Handles Guna2TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.FlatAppearance.BorderSize = 1
+        Button1.FlatAppearance.BorderColor = Color.Transparent
+        Button1.BackColor = Color.Transparent
+    End Sub
 End Class

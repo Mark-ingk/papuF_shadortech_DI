6f4d6baf6b1d602c7f36d87bc1686cac159f565c
hector123 <mcde345678@gmail.com>
2026-04-08 14:55:31 -0600

    ver si jala perfil

--- appPapuaFit/frmAcceso.vb
+++ appPapuaFit/frmAcceso.vb
@@ -2,4 +2,8 @@
     Private Sub Guna2HtmlLabel7_Click(sender As Object, e As EventArgs) Handles Guna2HtmlLabel7.Click
 
     End Sub
+
+    Private Sub btnGen_Click(sender As Object, e As EventArgs) Handles btnGen.Click
+
+    End Sub
 End Class
--- appPapuaFit/frmQR.Designer.vb
+++ appPapuaFit/frmQR.Designer.vb
@@ -22,15 +22,32 @@ Partial Class frmQR
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim CustomizableEdges1 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges2 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Guna2Panel1 = New Guna.UI2.WinForms.Guna2Panel()
         SuspendLayout()
+        ' 
+        ' Guna2Panel1
+        ' 
+        Guna2Panel1.Anchor = AnchorStyles.None
+        Guna2Panel1.CustomizableEdges = CustomizableEdges1
+        Guna2Panel1.Location = New Point(0, 0)
+        Guna2Panel1.MaximumSize = New Size(390, 844)
+        Guna2Panel1.MinimumSize = New Size(390, 844)
+        Guna2Panel1.Name = "Guna2Panel1"
+        Guna2Panel1.Padding = New Padding(25, 25, 25, 40)
+        Guna2Panel1.ShadowDecoration.CustomizableEdges = CustomizableEdges2
+        Guna2Panel1.Size = New Size(390, 844)
+        Guna2Panel1.TabIndex = 0
         ' 
         ' frmQR
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
-        AutoScaleMode = AutoScaleMode.Font
+        AutoScaleMode = AutoScaleMode.None
         BackColor = Color.FromArgb(CByte(15), CByte(15), CByte(15))
         ClientSize = New Size(390, 844)
+        Controls.Add(Guna2Panel1)
         FormBorderStyle = FormBorderStyle.None
+        Margin = New Padding(3, 2, 3, 2)
         MaximumSize = New Size(390, 844)
         MinimumSize = New Size(390, 844)
         Name = "frmQR"
@@ -38,4 +55,6 @@ Partial Class frmQR
         Text = "frmQR"
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents Guna2Panel1 As Guna.UI2.WinForms.Guna2Panel
 End Class

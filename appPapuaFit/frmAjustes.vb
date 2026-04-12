0428b9f3575ad50799761e37e8fe81920d8601aa
Arty <artimexx171@gmail.com>
2026-04-12 13:22:35 -0600

    interfaz de ajustes actualizada

--- appPapuaFit/frmAjustes.vb
+++ appPapuaFit/frmAjustes.vb
@@ -1,9 +1,9 @@
 ﻿Public Class frmAjustes
-    Private Sub Guna2CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles cbxNotifiacionesActivdadas.CheckedChanged
+    Private Sub Guna2CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles cbxNotifiacionesActivdadasAjustes.CheckedChanged
 
     End Sub
 
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblConfiguracionCuenta.Click
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblConfiguracionCuentaAjustes.Click
 
     End Sub
 
@@ -11,7 +11,11 @@
 
     End Sub
 
-    Private Sub lblPreferenciaAplicacion_Click(sender As Object, e As EventArgs) Handles lblPreferenciaAplicacion.Click
+    Private Sub lblPreferenciaAplicacion_Click(sender As Object, e As EventArgs) Handles lblPreferenciaAplicacionAjustes.Click
+
+    End Sub
+
+    Private Sub pnlAjustes_Paint_1(sender As Object, e As PaintEventArgs) Handles pnlAjustes.Paint
 
     End Sub
 End Class
